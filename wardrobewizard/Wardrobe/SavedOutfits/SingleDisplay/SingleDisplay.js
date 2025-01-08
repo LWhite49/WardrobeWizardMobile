b8640eca21fdc55a5b21df9461921e442639378a
Logan White <135063791+LWhite49@users.noreply.github.com>
2025-01-07 23:22:53 -0500

    Updated Saved Outfit Displat to use Gradient, and added clickable links to depop

--- wardrobewizard/Wardrobe/SavedOutfits/SingleDisplay/SingleDisplay.js
+++ wardrobewizard/Wardrobe/SavedOutfits/SingleDisplay/SingleDisplay.js
@@ -1,8 +1,8 @@
 import { SingleDisplayStyles } from "./SingleDisplayStyles";
-import { View } from "react-native";
+import { View, TouchableOpacity, Linking } from "react-native";
 import { Image } from "expo-image";
 import { useState } from "react";
-
+import { LinearGradient } from "expo-linear-gradient";
 export const SingleDisplay = (props) => {
 	const [display, setDisplay] = useState("none");
 
@@ -10,27 +10,49 @@ export const SingleDisplay = (props) => {
 		setDisplay("flex");
 	}, props.lockout);
 	return (
-		<View
-			style={{
-				...SingleDisplayStyles.container,
-				backgroundColor: "#" + props.top.productColors[0][0],
-				display: display,
-			}}>
-			<Image
-				style={SingleDisplayStyles.image}
-				source={props.topSrc}
-				priority="high"
-			/>
-			<Image
-				style={SingleDisplayStyles.image}
-				source={props.bottomSrc}
-				priority="high"
-			/>
-			<Image
-				style={SingleDisplayStyles.image}
-				source={props.shoesSrc}
-				priority="high"
-			/>
-		</View>
+		<LinearGradient
+			colors={[
+				"#" + props.top.productColors[0][0],
+				"#" + props.bottom.productColors[0][0],
+				"#" + props.shoes.productColors[0][0],
+			]}
+			style={SingleDisplayStyles.gradient}>
+			<View
+				style={{
+					...SingleDisplayStyles.container,
+					display: display,
+				}}>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL(props.top.productListing);
+					}}>
+					<Image
+						style={SingleDisplayStyles.image}
+						source={props.topSrc}
+						priority="high"
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL(props.bottom.productListing);
+					}}>
+					<Image
+						style={SingleDisplayStyles.image}
+						source={props.bottomSrc}
+						priority="high"
+					/>
+				</TouchableOpacity>
+				<TouchableOpacity
+					onPress={() => {
+						Linking.openURL(props.shoes.productListing);
+					}}>
+					<Image
+						style={SingleDisplayStyles.image}
+						source={props.shoesSrc}
+						priority="high"
+					/>
+				</TouchableOpacity>
+			</View>
+		</LinearGradient>
 	);
 };
--- wardrobewizard/Wardrobe/SavedOutfits/SingleDisplay/SingleDisplayStyles.js
+++ wardrobewizard/Wardrobe/SavedOutfits/SingleDisplay/SingleDisplayStyles.js
@@ -4,13 +4,18 @@ export const SingleDisplayStyles = StyleSheet.create({
 	container: {
 		display: "flex",
 		flexDirection: "column",
-		gap: 8,
+		gap: 12,
 		padding: 14,
 		borderRadius: 10,
 	},
 	image: {
-		width: 80,
-		height: 80,
+		width: 84,
+		height: 84,
 		borderRadius: 10,
+	},
+	gradient: {
+		height: "100%",
+		width: "100%",
+		borderRadius: 20,
 	},
 });
